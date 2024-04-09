2fbe3a68435fdddaf314ca6368d45cdf62db2381
Michael Zöller <michael2.zoeller@gmail.com>
2024-04-09 11:33:02 +0200

    Add solution

--- 02_java12_17/src/main/java/de/zettsystems/records/CustomerValidatingConstructor.java
+++ 02_java12_17/src/main/java/de/zettsystems/records/CustomerValidatingConstructor.java
@@ -1,13 +1,14 @@
 package de.zettsystems.records;
 
-import java.util.Objects;
+import lombok.NonNull;
 
-public record CustomerValidatingConstructor(String name, boolean vip) {
+public record CustomerValidatingConstructor(@NonNull String name,
+                                            boolean vip, byte[] arr) {
     public CustomerValidatingConstructor(String name) {
-        this(name, false);
+        this(name, false, null);
     }
 
-    public CustomerValidatingConstructor {
-        Objects.requireNonNull(name);
-    }
+//    public CustomerValidatingConstructor {
+//        Objects.requireNonNull(name);
+//    }
 }
